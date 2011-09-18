cc9bf15b9c8ec798c91c17e4b44cae9ab21e4322
AJ ONeal <coolaj86@gmail.com>
2011-09-18 07:24:56 +0400

    properly implemented udp redial

--- udp-echo.js
+++ udp-echo.js
@@ -7,6 +7,44 @@
     , server
     ;
 
+  function formatBody(data) {
+    var body;
+
+    body = data;
+
+    if (data.error) {
+      return data.error;
+    }
+
+    if ('string' !== typeof body) {
+      body = JSON.stringify(body);
+    }
+
+    return body;
+  }
+
+  function parseJson(data) {
+    var i = 0;
+
+    while (/\s/.exec(String.fromCharCode(data[i]))) {
+      i += 1;
+    }
+
+    if ('{' !== String.fromCharCode(data[i])) {
+      return;
+    }
+
+    try {
+      data = JSON.parse(data);
+    } catch(e) {
+      // ignore
+      console.log('\t[Stream] JSON Parse Error');
+      data = undefined;
+    }
+
+    return data;
+  }
+
   function onCreatedSocket() {
     console.log("[Listener] On Created Socket: port", port);
   }
@@ -22,7 +60,33 @@
 
   function onMessage(message, rinfo) {
     console.log("[Listener] On Message: " + message + " from " + rinfo.address + ":" + rinfo.port);
-    server.send(message, 0, message.length, rinfo.port, rinfo.address, onSent);
+
+    var json
+      , client
+      ;
+
+    function onRedial() {
+      var message = new Buffer('[Info] Sent message to ' + rinfo.address + ':' + rinfo.port + '\n');
+      server.send(message, 0, message.length, rinfo.port, rinfo.address, onSent);
+    }
+
+    json = parseJson(message);
+
+    if (!json) {
+      server.send(message, 0, message.length, rinfo.port, rinfo.address, onSent);
+      return;
+    }
+
+    json.body = new Buffer(json.body && formatBody(json.body) || '[Error] must specify `body` to send\n');
+    //console.log('JSON', json.body, json.body.length);
+
+    if (json.redial) {
+      // TODO On Error
+      client = dgram.createSocket("udp4", onClientCreatedSocket);
+      client.send(json.body, 0, json.body.length, parseInt(json.port) || rinfo.port, rinfo.address, onRedial);
+    } else {
+      server.send(json.body, 0, json.body.length, parseInt(json.port) || rinfo.port, rinfo.address, onSent);
+    }
   }
 
   function onListening() {
